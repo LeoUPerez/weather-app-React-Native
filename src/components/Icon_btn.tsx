6a96e0cf21de8e551d81ed4a2b23db6b67d5118f
Leo <leo0223perez@outlook.com>
2023-10-04 07:06:32 -0400

    refactoring and making the component reusable

--- src/components/Icon_btn.tsx
+++ src/components/Icon_btn.tsx
@@ -1,24 +1,42 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useContext } from "react";
 import { globalContext } from "../context/global_Context";
 
-export default function Custom_btn() {
+type PropsType = {
+  text: string;
+  name: "search" | "chevron-forward-outline";
+};
+
+export default function Icon_btn({ text, name }: PropsType) {
   const Context = useContext(globalContext);
 
   return (
-    <Ionicons
-      onPress={() => {
-        Context.fetchCity();
-      }}
-      style={styles.icon_button}
-      name="search"
-    ></Ionicons>
+    <View style={styles.container_btn}>
+      <Text style={styles.text_style}>{text}</Text>
+      <Ionicons
+        onPress={() => {
+          // Context.fetchCity();
+        }}
+        style={styles.icon_button}
+        name={name}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   icon_button: {
     fontSize: 25,
+    color: "rgb(33, 97, 140)"
+  },
+  container_btn: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text_style: {
+    fontWeight: "bold",
+    color: "rgb(33, 97, 140)",
   },
 });
