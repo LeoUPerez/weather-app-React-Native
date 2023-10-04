68f8c0ba062bb947f91e68e53409964995123d91
Leo <leo0223perez@outlook.com>
2023-10-04 07:05:05 -0400

    refactoring and adding new parts of the view

--- src/views/home_View.tsx
+++ src/views/home_View.tsx
@@ -1,34 +1,25 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
-import Header from "../components/Header";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { globalContext } from "../context/global_Context";
 import { useContext } from "react";
+import Icon_info from "../components/Icon_info";
+import Forecast_card_info from "../components/Forecast_card_info";
+import Header from "../components/Header";
+import Icon_btn from "../components/Icon_btn";
 
 export default function HomeView() {
   const Context = useContext(globalContext);
 
   return (
-    // <ScrollView>
     <View style={styles.temp_container}>
-      <Header />
+      {/* <Header /> */}
       <LinearGradient
         colors={["rgba(69,121,241,255)", "transparent"]}
         end={{ x: 2, y: 0 }}
         start={{ x: 0, y: 1 }}
         style={styles.gradient_box}
       >
-        <Text
-          style={{
-            fontWeight: "900",
-            fontSize: 140,
-            textAlign: "right",
-            color: "rgba(255, 255, 255, .9)",
-            opacity: 0.8,
-            paddingHorizontal: 5,
-            marginTop: "-10%",
-          }}
-        >
+        <Text style={styles.temp_text_style}>
           {Context?.city != undefined
             ? Context?.city?.main.temp.toString().split(".")[0]
             : 0}
@@ -40,12 +31,40 @@ export default function HomeView() {
         />
       </LinearGradient>
       <View style={styles.prueba}>
-        <Ionicons name="umbrella" size={50} />
-        <Ionicons name="water" size={50} />
-        <Ionicons name="md-cloud" size={50} />
+        <Icon_info
+          name="thermometer"
+          text="Temp. max"
+          defaultData="0"
+          data={Context.city!?.main.temp_max.toString().split(".")[0]}
+        />
+        <Icon_info
+          name="water"
+          text="Humidity"
+          defaultData="0"
+          data={Context.city!?.main.humidity}
+        />
+        <Icon_info
+          name="md-cloud"
+          text="Wind speed"
+          defaultData="0"
+          data={Context.city!?.wind.speed}
+        />
+      </View>
+      <View style={styles.container_forecast_style}>
+        <View style={styles.header_forecast_style}>
+          <Text style={{fontWeight: "bold", color: "rgb(33, 97, 140)"}}>Today</Text>
+          <Icon_btn text="5 day weather forecast" name="chevron-forward-outline"/>
+        </View>
+        <ScrollView horizontal={true} style={styles.container_card_forecast_style}>
+          <Forecast_card_info />
+          <Forecast_card_info />
+          <Forecast_card_info />
+          <Forecast_card_info />
+          <Forecast_card_info />
+
+        </ScrollView>
       </View>
     </View>
-    //  </ScrollView>
   );
 }
 
@@ -53,11 +72,18 @@ const styles = StyleSheet.create({
   temp_container: {
     width: "100%",
     height: "45%",
-    borderWidth: 2,
     display: "flex",
-    // justifyContent: "center",
     alignItems: "center",
     gap: 10,
+  },
+  temp_text_style: {
+    fontWeight: "900",
+    fontSize: 140,
+    textAlign: "right",
+    color: "rgba(255, 255, 255, .9)",
+    opacity: 0.8,
+    paddingHorizontal: 5,
+    marginTop: "-10%",
   },
   gradient_box: {
     width: "70%",
@@ -65,28 +91,46 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginTop: "3%",
     position: "relative",
-    // paddingHorizontal: "3.5%",
   },
   prueba: {
     width: "80%",
     height: "30%",
     backgroundColor: "white",
     borderRadius: 15,
-    // padding: "3.5%",
     elevation: 10,
     display: "flex",
     flexDirection: "row",
-    // shadowOffset: {
-    //   height: 100,
-    //   width: 100
-    // },
+    justifyContent: "space-around",
     shadowColor: "black",
   },
   image: {
     position: "absolute",
     bottom: "-10%",
     left: "-15%",
-    width: 120,
-    height: 120,
+    width: 150,
+    height: 150,
+  },
+  container_forecast_style: {
+    width: "100%",
+    height: 170,
+    // backgroundColor: "blue",
+    padding: 15,
+    // borderWidth: .4
+  },
+  header_forecast_style: {
+    width: "100%",
+    // backgroundColor: "pink",
+    // padding: 15,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  container_card_forecast_style: {
+    width: "100%",
+    // height: "100%",
+    // backgroundColor: "red",
+    // display: "flex",
+    // gap: 5,
+    // flexDirection: "row"
   },
 });
