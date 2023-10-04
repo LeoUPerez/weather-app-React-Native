9f6d731c9826a0f0a5125cbaf82b315aed6357fe
Leo <leo0223perez@outlook.com>
2023-10-04 07:03:37 -0400

    developing new component

---
+++ src/components/Forecast_card_info.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from "react-native";
+
+
+export default function Forecast_card_info() {
+    return (
+        <View style={styles.card_style}>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    card_style:{
+        width: 85,
+        height: "100%",
+        marginHorizontal: 4,
+        backgroundColor: "yellow",
+        borderRadius: 15
+    }
+})
