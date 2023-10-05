bcc34b243e8b548b10862e531fa2acfd64aeb56a
Leo Ernesto <leo0223perez@outlook.com>
2023-10-05 12:04:19 -0300

    performing tests to see if obtaining the forecast works

--- src/components/ForecastCardInfo.tsx
+++ src/components/ForecastCardInfo.tsx
@@ -1,19 +1,25 @@
-import { StyleSheet, View } from "react-native";
+import { ListRenderItemInfo, StyleSheet, Text, View } from "react-native";
+import { City } from "../models/CityModel";
+import { List } from "../models/ForecastModel";
 
+type PropsType = {
+    data: ListRenderItemInfo<List>
+}
 
-export default function Forecast_card_info() {
+export default function Forecast_card_info({data} : PropsType) {
+    
     return (
         <View style={styles.card_style}>
-
+            <Text style={{color: "white"}}>{data.item.main.temp}</Text>
         </View>
     )
 }
-
 
 const styles = StyleSheet.create({
     card_style:{
         width: 85,
         height: 110,
+        padding: 3,
         marginHorizontal: 4,
         backgroundColor: "blue",
         borderRadius: 15
