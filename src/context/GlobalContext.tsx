589246649191d45349e02063858806e9b716dbd0
Leo <leo0223perez@outlook.com>
2023-10-06 08:33:17 -0400

    creating method to get city by latency and length

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -14,7 +14,8 @@ type ContextProviderProps = {
 };
 
 type ContextType = {
-  fetchCity: () => void;
+  fetchCityName: () => void;
+  fetchCityLatLon: (lat: number, lon: number) => void;
   forecastCity: Forecast | undefined;
   FetchForecastCity: (lat: number, lon: number) => void;
   city?: City;
@@ -28,10 +29,11 @@ export const ContexProvider = ({ children }: ContextProviderProps) => {
   const lat = useRef();
   const lon = useRef();
   const [searchCity, setSearchCity] = useState("");
+  const [load, setLoad] = useState(false);
   const [city, setCity] = useState<City>();
   const [forecastCity, setForecastCity] = useState<Forecast>();
 
-  const fetchCity = () => {
+  const fetchCityName = () => {
     const APIID = "8926fd8755940c0fb62183daa7f7ebe6";
     // console.log(searchCity);
 
@@ -50,17 +52,46 @@ export const ContexProvider = ({ children }: ContextProviderProps) => {
       .catch((err) => {
         console.log(err);
       });
-
     // console.log(city!?.coord!?.lat, city!?.coord!?.lon, APIID);
       // city?.weather[0].id
       // console.log(data["main"]["temp"])
     //! city?.weather[0].main
   };
 
+  const fetchCityLatLon = (lat: number, lon: number) => {
+    if (load != true) {
+      const APIID = "8926fd8755940c0fb62183daa7f7ebe6";
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIID}&units=metric`
+      )
+        .then(async (response) => {
+          if (response.ok) {
+            var city: City = await response.json();
+            setCity(city);
+            FetchForecastCity(city.coord.lat, city.coord.lon);
+          } else {
+            Alert.alert("Weather App Alert", "City not found");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+        console.log("se hizo una vez");
+        
+    }
+    // else{
+    //   console.log("no se hizo");
+
+    // }
+      setLoad(true);
+  };
+
+
   return (
     <globalContext.Provider
       value={{
-        fetchCity,
+        fetchCityName,
+        fetchCityLatLon,
         FetchForecastCity,
         searchCity,
         city,
@@ -71,6 +102,7 @@ export const ContexProvider = ({ children }: ContextProviderProps) => {
       {children}
     </globalContext.Provider>
   );
+
   function FetchForecastCity(lat: number, lon: number) {
     const APIID = "8926fd8755940c0fb62183daa7f7ebe6";
     fetch(
