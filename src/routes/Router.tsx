e4a7721aa830c92411e88535fbff1e1d3fca1dbd
Leo Ernesto <leo0223perez@outlook.com>
2023-10-09 09:41:43 -0400

    system of routes

---
+++ src/routes/Router.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeView from "../views/Home";
+import CitiesFavView from "../views/FavoriteCities";
+
+const Stack = createNativeStackNavigator<IRouteName>();
+
+export const Routes = {
+    FavoriteCity: 'FavoriteCity',
+    Home: 'Home'
+}
+
+export default function RouterNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={Routes.Home}>
+        <Stack.Screen name={Routes.Home} component={HomeView} />
+        <Stack.Screen name={Routes.FavoriteCity} component={CitiesFavView} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
