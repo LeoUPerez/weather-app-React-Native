c29efd3dd960f6140741b04eeed8ebb3c48905af
Leo <leo0223perez@outlook.com>
2023-10-17 09:44:50 -0400

    refactoring

--- src/components/IconBtn.tsx
+++ src/components/IconBtn.tsx
@@ -3,44 +3,21 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { useContext } from "react";
 import { globalContext } from "../context/GlobalContext";
 import { useNavigation } from "@react-navigation/native";
-// import { Routes } from "../routes/Router";
-
-import {
-  NativeStackNavigationProp,
-  NativeStackScreenProps,
-} from "@react-navigation/native-stack";
-import { RootStackPramList } from "../@types/stack-navigation";
-// import { navigate } from "../routes/Router";
 
 interface PropsType {
   text: string;
   name: "search" | "chevron-forward-outline";
-  func: () => void
+  func: () => void;
 }
-// export type NavigationProps = NativeStackScreenProps<RootStackPramList>;
 
 function IconBtn({ text, name, func }: PropsType) {
-  // const navi = useNavigation<NativeStackNavigationProp<RootStackPramList>>();
-
-  const Context = useContext(globalContext);
-
   return (
-    <TouchableOpacity
-      style={styles.ContainerBtn}
-      onPress={() => func()} //navi.navigate("FavoriteCity")
-    >
+    <TouchableOpacity style={styles.ContainerBtn} onPress={() => func()}>
       <Text style={styles.TextStyle}>{text}</Text>
       <Ionicons name={name} style={styles.IconButton}></Ionicons>
     </TouchableOpacity>
   );
 }
-
-// IconBtn.displayName = "IconBtn";
-
-// IconBtn.propTypes = {
-//   text: PropTypes.string.isRequired,
-//   name: PropTypes.oneOf(["search", "chevron-forward-outline"]),
-// };
 
 const styles = StyleSheet.create({
   IconButton: {
