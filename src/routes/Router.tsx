4c6cd54783710ff0aede0ad38e66341778015403
Leo <leo0223perez@outlook.com>
2023-10-17 11:23:16 -0400

    refactoring

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,26 +1,26 @@
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { NativeStackNavigationProp, NativeStackScreenProps, createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  NativeStackNavigationProp,
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
 import { RootStackPramList } from "../@types/stack-navigation";
 import { HomeView } from "../views/Home";
 import { CitiesFavView } from "../views/FavoriteCities";
 
-
-// export const Routes = {
-//   FavoriteCity: "FavoriteCity",
-//   Home: "Home",
-// };
-
-
 const Stack = createNativeStackNavigator<RootStackPramList>();
-
 
 export default function RouterNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen options={{headerShown:false, }} name='Home' component={HomeView} />
-        <Stack.Screen name='FavoriteCity' component={CitiesFavView} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={HomeView}
+        />
+        <Stack.Screen name="FavoriteCity" component={CitiesFavView} />
       </Stack.Navigator>
-     </NavigationContainer>
+    </NavigationContainer>
   );
 }
