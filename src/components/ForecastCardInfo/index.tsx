ae5c19c25145c82e985ce800077869fe0ad2d98f
Leo Ernesto <leo0223perez@outlook.com>
2023-10-19 11:36:17 -0400

    adding ternary to select images based on weather, refactoring and indexation

--- src/components/ForecastCardInfo/index.tsx
+++ src/components/ForecastCardInfo/index.tsx
@@ -1,40 +1,49 @@
-import { ListRenderItemInfo, Text, Image } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { styles } from "./style";
-import { CardForecast } from "../../models/CardForecastModel";
+import {ListRenderItemInfo, Text, Image} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import {styles} from "./style";
+import {CardForecast} from "../../models/CardForecastModel";
 
 interface PropsType {
-  data: ListRenderItemInfo<CardForecast>;
+    data: ListRenderItemInfo<CardForecast>;
 }
 
-export default function ForecastCardInfo({ data }: PropsType) {
-  const { weather } = data.item;
+export default function ForecastCardInfo({data}: PropsType) {
+    const {weather} = data.item;
 
-  return (
-    <LinearGradient
-      style={styles.card_style}
-      colors={["rgba(69,121,241,255)", "transparent"]}
-      end={{ x: 2.8, y: 0 }}
-      start={{ x: 2, y: 1 }}
-    >
-      <Text style={{ color: "white", fontWeight: "bold", fontSize: 10 }}>
-        {data.item.hour}
-      </Text>
+    return (
+        <LinearGradient
+            style={styles.card_style}
+            colors={["rgba(69,121,241,255)", "transparent"]}
+            end={{x: 2.8, y: 0}}
+            start={{x: 2, y: 1}}
+        >
+            <Text style={{color: "white", fontWeight: "bold", fontSize: 10}}>
+                {data.item.hour}
+            </Text>
+            <Text style={{color: "white", fontWeight: "bold", fontSize: 10}}>
+                {data.item.date}
+            </Text>
 
-      <Image
-        style={styles.Image}
-        source={
-          weather == "Clouds"
-            ? require("../../assets/images/clouds.png")
-            : data.item.weather == "Rain"
-            ? require("../../assets/images/rain.png")
-            : require("../../assets/images/clear.png")
-        }
-      />
-      {/* <Text>{data.item.weather}</Text> */}
-      <Text style={{ color: "white", fontWeight: "bold" }}>
-        {data.item.temp} C°
-      </Text>
-    </LinearGradient>
-  );
+            <Image
+                style={styles.Image}
+                source={
+                    weather == "Clear" ?
+                        require("../../assets/images/clouds.png")
+                        : data.item.weather == "Clouds"
+                            ? require("../../assets/images/clouds.png")
+                            : data.item.weather == "Drizzle"
+                                ? require("../../assets/images/drizzle.png")
+                                : data.item.weather == "Mist"
+                                    ? require("../../assets/images/mist.png")
+                                    : data.item.weather == "Rain"
+                                        ? require("../../assets/images/rain.png")
+                                        : require("../../assets/images/snow.png")
+                }
+            />
+            {/* <Text>{data.item.weather}</Text> */}
+            <Text style={{color: "white", fontWeight: "bold"}}>
+                {data.item.temp} C°
+            </Text>
+        </LinearGradient>
+    );
 }
