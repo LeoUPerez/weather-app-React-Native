a2c81c25fa192028129416dcf5d83cd6a54dea6f
Leo <leo0223perez@outlook.com>
2023-10-22 13:50:41 -0400

    new component for avatar of user

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,29 @@
+import {View, Text, TouchableOpacity, Button, Image} from "react-native";
+import {styles} from "./style";
+import * as ImagePicker from "expo-image-picker";
+import {SetStateAction, useState} from "react";
+
+
+export default function Avatar() {
+    const [image, setImage] = useState<SetStateAction<any>>("");
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0]!.uri);
+        }
+    };
+
+    return (
+        <TouchableOpacity style={styles.Avatar} onPress={pickImage}>
+            <Image source={!image ? require("../../assets/images/userDefault.jpg") : {uri: image}}
+                   style={styles.Image}/>
+        </TouchableOpacity>
+    )
+}
---
+++ src/components/Avatar/style.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    Avatar: {
+        width: 50,
+        height: 60,
+        display: "flex",
+        justifyContent: "center",
+    },
+    Image: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+    }
+});
