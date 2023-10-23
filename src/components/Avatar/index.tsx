ac1f5bbe589f50f02d53c462c01ee308e726b50b
Leo <leo0223perez@outlook.com>
2023-10-22 20:58:40 -0400

    only permit images for upload

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -9,7 +9,7 @@ export default function Avatar() {
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
