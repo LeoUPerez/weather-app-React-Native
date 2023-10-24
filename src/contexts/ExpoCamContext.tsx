e49d082f74ff5e0b1c7c495d6def827c7603a35c
Leo <leo0223perez@outlook.com>
2023-10-23 23:45:36 -0400

    Added a new context to be consumed by the camera and Avatar

---
+++ src/contexts/ExpoCamContext.tsx
@@ -0,0 +1,84 @@
+import {createContext, Dispatch, SetStateAction, useContext, useRef, useState} from "react";
+import * as ImagePicker from "expo-image-picker";
+import {weatherContext} from "./WeatherContext";
+import {Camera} from "expo-camera";
+import {Alert} from "react-native";
+
+interface ContextType {
+    searchPicGallery: () => void;
+    getPermissionsCam: () => void;
+    camera: any;
+    takePicture: () => void;
+    selectActions: () => void;
+    stateCamera: boolean;
+    setStateCamera: Dispatch<SetStateAction<boolean>>;
+    setImage: Dispatch<SetStateAction<string>>;
+    image: string;
+}
+
+export const expoCamContext = createContext<ContextType>({} as any);
+
+export const ExpoCamContextProvider = ({children}: any) => {
+    const [stateCamera, setStateCamera] = useState(false);
+    const [image, setImage] = useState<SetStateAction<any>>("");
+    const camera: any = useRef();
+
+    async function searchPicGallery() {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0]!.uri);
+            // uploadImageDB();
+        }
+    }
+
+    async function getPermissionsCam() {
+        await Camera.requestCameraPermissionsAsync().then(({granted}) => {
+            if (granted) {
+                setStateCamera(true);
+            }
+        });
+    }
+
+    function selectActions() {
+        Alert.alert("", "What do you want?", [
+            {
+                text: 'Search photo in gallery',
+                onPress: () => searchPicGallery(),
+            },
+            {
+                text: 'Take photo',
+                onPress: async () => getPermissionsCam(),
+            },
+        ])
+    }
+
+    function takePicture() {
+        camera.current.takePictureAsync().then((data: any) => {
+            setImage(data.uri);
+            setStateCamera(false)
+        });
+    }
+
+    return (
+        <expoCamContext.Provider
+            value={{
+                camera,
+                image,
+                setImage,
+                setStateCamera,
+                stateCamera,
+                searchPicGallery,
+                getPermissionsCam,
+                selectActions,
+                takePicture
+            }}>
+            {children}
+        </expoCamContext.Provider>
+    )
+}
