06abf8d51d4ceb6c61d32579b41c5fb5995a81e1
Leo <leo0223perez@outlook.com>
2023-10-23 23:38:45 -0400

    separating of logic of the style

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,60 +1,15 @@
-import {Alert, Image, Text, TouchableOpacity} from "react-native";
+import {Alert, Image, TouchableOpacity} from "react-native";
 import {styles} from "./style";
-import * as ImagePicker from "expo-image-picker";
-import {SetStateAction, useRef, useState} from "react";
-import {createClient} from '@supabase/supabase-js'
-import {Camera, CameraType} from 'expo-camera'
-
+import {useContext} from "react";
+import {expoCamContext} from "../../contexts/ExpoCamContext";
 
 export default function Avatar() {
-    const [image, setImage] = useState<SetStateAction<any>>("");
-    const [stateCamera, setStateCamera] = useState(false)
-    const camera: any = useRef();
 
-
-    const pickImage = async () => {
-
-        Alert.alert("", "¿Que desea?", [
-            {
-                text: 'Buscar foto en galeria',
-                onPress: async () => {
-                    let result = await ImagePicker.launchImageLibraryAsync({
-                        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                        allowsEditing: true,
-                        aspect: [4, 3],
-                        quality: 1,
-                    });
-
-                    if (!result.canceled) {
-                        setImage(result.assets[0]!.uri);
-                        // uploadImageDB();
-                    }
-                },
-            },
-            {
-                text: 'Tomar foto',
-                onPress: async () => {
-                    await Camera.requestCameraPermissionsAsync().then(({granted}) => {
-                        if (granted) {
-                            setStateCamera(true);
-                        }
-                    });
-
-                },
-            },
-        ])
-    };
-
+    const Context = useContext(expoCamContext);
 
     return (
-        <TouchableOpacity style={styles.Avatar} onPress={pickImage}>
-            {stateCamera &&
-                <Camera ref={camera} style={{width: 200, height: 200}} type={CameraType.front} onCameraReady={() => {
-                }}/>}
-            {stateCamera && <TouchableOpacity onPress={() => {
-                setStateCamera(false)
-            }}><Text>Close</Text></TouchableOpacity>}
-            <Image source={!image ? require("../../assets/images/userDefault.jpg") : {uri: image}}
+        <TouchableOpacity style={styles.Avatar} onPress={Context.selectActions}>
+            <Image source={!Context.image ? require("../../assets/images/userDefault.jpg") : {uri: Context.image}}
                    style={styles.Image}/>
         </TouchableOpacity>
     )
