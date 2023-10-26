0af229a39f5175e7c04044e140c9a514c6535718
Leo <leo0223perez@outlook.com>
2023-10-26 15:22:45 -0400

    remove not use

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import {View, StyleSheet} from "react-native";
+import {View} from "react-native";
 import {useContext} from "react";
 import {weatherContext} from "../../contexts/WeatherContext";
 import {styles} from "./style";
