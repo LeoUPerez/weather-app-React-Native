37ec70ca55668a1b6cb18e88ae152ed6768feec3
Leo Ernesto <leo0223perez@outlook.com>
2023-11-29 17:03:26 -0400

    Bug resolved (when the city was not a favorite, the star was not clicked, it now does so without problem).

--- src/components/FavoriteStar/FavoriteStar.tsx
+++ src/components/FavoriteStar/FavoriteStar.tsx
@@ -2,6 +2,7 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import {useContext, useEffect} from "react";
 import {dataBaseContext} from "../../contexts/DataBaseContext";
 import {weatherContext} from "../../contexts/WeatherContext";
+import {TouchableOpacity} from "react-native";
 
 export default function FavoriteStar() {
     const ContextDB = useContext(dataBaseContext);
@@ -12,7 +13,11 @@ export default function FavoriteStar() {
     }, [Context.city!?.name]);
 
     return (
-        <Ionicons name={ContextDB.favorite ? "star" : "star-outline"} style={{}} size={30}
-                  color={ContextDB.favorite ? "rgb(241, 196, 15 )" : "rgba(23, 32, 42, .5)"}/>
+        <TouchableOpacity style={{zIndex: 1}}
+                          onPress={() => ContextDB.UpdateFavoriteCities(ContextDB.favorite, Context.city!?.name)}>
+            <Ionicons
+                name={ContextDB.favorite ? "star" : "star-outline"} style={{zIndex: 0}} size={30}
+                color={ContextDB.favorite ? "rgb(241, 196, 15 )" : "rgba(23, 32, 42, .5)"}/>
+        </TouchableOpacity>
     )
 }
