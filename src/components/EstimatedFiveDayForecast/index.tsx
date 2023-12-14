98cb6f12bcf19773976fe05eab73cdce7e146daa
Leo Ernesto <leo0223perez@outlook.com>
2023-12-14 10:13:54 -0400

    added skeleton loader

--- src/components/EstimatedFiveDayForecast/index.tsx
+++ src/components/EstimatedFiveDayForecast/index.tsx
@@ -2,6 +2,7 @@ import {ActivityIndicator, FlatList, View} from "react-native";
 import ForecastCardInfo from "../ForecastCardInfo";
 import {useContext, useEffect} from "react";
 import {weatherContext} from "../../contexts/WeatherContext";
+import {ForecastCardInfoSkeleton} from "../Skeletons";
 
 export default function EstimatedFiveDayForecast() {
     const Context = useContext(weatherContext);
@@ -15,12 +16,20 @@ export default function EstimatedFiveDayForecast() {
     return (
         <View>
             {Context.loading ? (
-                <ActivityIndicator size={50}/>
+                    <FlatList
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+                        ItemSeparatorComponent={() => <View style={{width: 4.5}}/>}
+                        renderItem={() => <ForecastCardInfoSkeleton/>}
+                    />
+
             ) : (
                 <FlatList
                     horizontal={true}
                     showsHorizontalScrollIndicator={false}
                     data={Context.city?.estimateFiveDays}
+                    ItemSeparatorComponent={() => <View style={{width: 4.5}}/>}
                     renderItem={(item) => <ForecastCardInfo data={item}/>}
                 />
             )}
