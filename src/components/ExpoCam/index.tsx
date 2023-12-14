85d299e31bd661a93d3a281078843b47641423f1
Leo Ernesto <leo0223perez@outlook.com>
2023-12-14 10:10:10 -0400

    refactoring style and added new methof for cancel picture

--- src/components/ExpoCam/index.tsx
+++ src/components/ExpoCam/index.tsx
@@ -1,19 +1,45 @@
 import {useContext} from "react";
 import {Camera, CameraType} from "expo-camera";
-import {Text, TouchableOpacity, View} from "react-native";
+import {Dimensions, Text, TouchableOpacity, View} from "react-native";
 import {expoCamContext} from "../../contexts/ExpoCamContext";
 import {style} from "./style";
 
 export default function ExpoCam() {
     const Context = useContext(expoCamContext);
+    const windowHei = Dimensions.get("window").height;
 
     return (
-        <View style={Context.stateCamera ? style.cameraON : style.cameraOFF}>
+        <TouchableOpacity style={{
+            position: "absolute",
+            zIndex: 6,
+            width: "100%",
+            height: windowHei,
+            display: Context.stateCamera ? "flex" : "none",
+            backgroundColor: "transparent",
+            alignItems: "center",
+            gap: 5,
+            paddingTop: 30,
+        }}
+        onPress={() => Context.cancelPicture()}
+        >
+            <View style={Context.stateCamera ? style.cameraON : style.cameraOFF}>
+                {Context.stateCamera &&
+                    <Camera ref={Context.camera} style={{width: "100%", height: 450}} type={CameraType.front}/>}
+            </View>
             {Context.stateCamera &&
-                <Camera ref={Context.camera} style={{width: '100%', height: '90%'}} type={CameraType.front}/>}
-            {Context.stateCamera &&
-                <TouchableOpacity onPress={() => Context.takePicture()}><Text style={style.textBtn}>Take a
-                    photo</Text></TouchableOpacity>}
-        </View>
+                <View style={{
+                    width: "100%",
+                    display: "flex",
+                    gap: 10,
+                    justifyContent: "center",
+                    flexDirection: "row",
+                }}>
+                    <TouchableOpacity onPress={() => Context.takePicture()}><Text style={style.takePhoto}>Take a
+                        photo</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={() => Context.cancelPicture()}><Text style={style.cancelPhoto}>Cancel Photo</Text></TouchableOpacity>
+                </View>
+
+            }
+        </TouchableOpacity>
     )
 };
