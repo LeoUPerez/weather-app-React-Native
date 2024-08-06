7f5ba0ecf92ac5fddab67bdaeaa0e1cd7b095278
Leo <leo0223perez@outlook.com>
2024-08-06 15:08:49 -0400

    apply style to work in web

--- src/components/EstimatedFiveDayForecast/index.tsx
+++ src/components/EstimatedFiveDayForecast/index.tsx
@@ -22,8 +22,8 @@ export default function EstimatedFiveDayForecast() {
                         showsHorizontalScrollIndicator={false}
                         data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
                         ItemSeparatorComponent={() => <View style={{width: 4.5}}/>}
-                        renderItem={() => <>Cargando</>}
-                        // renderItem={() => <ForecastCardInfoSkeleton/>}
+                        // renderItem={() => <>Cargando</>}
+                        renderItem={() => <ForecastCardInfoSkeleton/>}
                     />
 
             ) : (
--- src/components/Skeletons/index.tsx
+++ src/components/Skeletons/index.tsx
@@ -5,30 +5,32 @@ import {weatherContext} from "../../contexts";
 export function ForecastCardInfoSkeleton(){
     const Context = useContext(weatherContext);
     return(
-        <Skeleton
-            show={Context.loading}
-            width={85}
-            height={110}
-            colorMode="light"
-            transition={{
-                type: 'timing',
-                duration: 3000,
-            }}
-        />
+        <> Cargando </>
+        // <Skeleton
+        //     show={Context.loading}
+        //     width={85}
+        //     height={110}
+        //     colorMode="light"
+        //     transition={{
+        //         type: 'timing',
+        //         duration: 3000,
+        //     }}
+        // />
     )
 }
 
 export default function FavoriteCityItemSkeleton({horizontal}: {horizontal: boolean}){
     return(
-        <Skeleton
-            show={true}
-            width={horizontal ? 175 : "97%"}
-            height={horizontal ? "100%" : 100}
-            colorMode="light"
-            transition={{
-                type: 'timing',
-                duration: 3000,
-            }}
-        />
+        <></>
+        // <Skeleton
+        //     show={true}
+        //     width={horizontal ? 175 : "97%"}
+        //     height={horizontal ? "100%" : 100}
+        //     colorMode="light"
+        //     transition={{
+        //         type: 'timing',
+        //         duration: 3000,
+        //     }}
+        // />
     )
 }
